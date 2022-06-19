00a433bf21a978fcbe5ea8ede1cd0cfe80cf8fef
goldenhub <goldenazubuike@gmail.com>
2022-06-19 05:50:23 +0100

    edited db config file

--- web/api/index.php
+++ web/api/index.php
@@ -2,4 +2,31 @@
 
 include_once 'dbconn.php';
 
-echo "Hello World";
+class test extends Conn
+{
+    public function test()
+    {
+        $conn = $this->connect();
+
+        $sql = "SELECT * FROM interns";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+            $this->close();
+            return json_encode(array("statuscode" => 0, "status" => "request was successful", "data" => $data));
+        } else {
+            $this->close();
+            return json_encode(array("statuscode" => 1, "status" => "request was successful", "data" => "No record found"));
+        }
+    }
+
+    public function result()
+    {
+        return $this->test();
+    }
+}
+
+$res = new test();
+echo $res->result();
