eaa0bd3e3d2a958f8c4e899ede7bd6ab4e4e9018
goldenhub <goldenazubuike@gmail.com>
2022-06-22 07:22:04 +0100

    updated

--- api/dbConn.php
+++ api/dbConn.php
@@ -9,20 +9,20 @@ class Conn
     private $db;
 
     private $conn;
-    // mysql://b41d2a89fb3b24:42df1f6b@eu-cdbr-west-02.cleardb.net/heroku_2e7391170fbe8e4?reconnect=true
+    
     protected function connect()
     {
-        $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $this->host = $cleardb_url["host"];
-        $this->user = $cleardb_url["user"];
-        $this->password = $cleardb_url["pass"];
-        $this->db = substr($cleardb_url["path"],1);
-
-        if ($_SERVER['HTTP_HOST'] == 'localhost') {
-            $this->host = "localhost";
-            $this->user = "root";
-            $this->password = "";
-            $this->db = "cinterns";
+        if($_SERVER['HTTP_HOST'] != 'localhost'){
+            $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $this->host = $cleardb_url["host"];
+            $this->user = $cleardb_url["user"];
+            $this->pass = $cleardb_url["pass"];
+            $this->db = substr($cleardb_url["path"],1);
+        }else{
+            $this->host = 'localhost';
+            $this->user = 'root';
+            $this->pass = '';
+            $this->db = 'cinterns';
         }
 
         try{
--- api/getInterns.php
+++ api/getInterns.php
@@ -9,7 +9,7 @@ class GetIntern extends Conn
     {
         $conn = $this->connect();
         
-        $sql = "USE cinterns; SELECT `id`, `name`, `email`, `school`, `major`, `city`, `state`, `country`, `github`, `linkedin`, `skills`, `experience`, `image` FROM interns ORDER BY id ASC";
+        $sql = "SELECT `id`, `name`, `email`, `school`, `major`, `city`, `state`, `country`, `github`, `linkedin`, `skills`, `experience`, `image` FROM interns ORDER BY id ASC";
 
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
@@ -20,7 +20,7 @@ class GetIntern extends Conn
             return json_encode(array("statuscode" => 0, "status" => "request was successful", "data" => $data));
         } else {
             $this->close();
-            return json_encode(array("statuscode" => 1, "status" => "request was successful", "data" => "No record found"));
+            return json_encode(array("statuscode" => 0, "status" => "request was successful", "data" => "No record found"));
         }
     }
 
