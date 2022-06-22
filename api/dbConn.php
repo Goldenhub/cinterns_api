0d5575f9d6b3ce5c18b0591655c85649857d6ef4
goldenhub <goldenazubuike@gmail.com>
2022-06-22 07:50:34 +0100

    updated

--- api/dbConn.php
+++ api/dbConn.php
@@ -15,32 +15,26 @@ class Conn
 
     protected function connect()
     {
-        if($_SERVER['HTTP_HOST'] != 'localhost'){
+        if ($_SERVER['HTTP_HOST'] != 'localhost') {
             $this->cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
             $this->host = $this->cleardb_url["host"];
             $this->user = $this->cleardb_url["user"];
             $this->pass = $this->cleardb_url["pass"];
-            $this->db = substr($this->cleardb_url["path"],1);
+            $this->db = substr($this->cleardb_url["path"], 1);
             $this->active_group = 'default';
             $this->query_builder = TRUE;
-        }else{
+        } else {
             $this->host = 'localhost';
             $this->user = 'root';
             $this->pass = '';
             $this->db = 'cinterns';
         }
 
-        try{
 
-            $conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
-            $this->conn = $conn;
-        }catch(Exception $e){
-            var_dump(phpinfo());
-            echo "Error: " . $e->getMessage();
-            die();
-        }
+        $conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
+        $this->conn = $conn;
+
         return $this->conn;
-
     }
 
     protected function close()
@@ -48,4 +42,3 @@ class Conn
         $this->conn->close();
     }
 }
-
