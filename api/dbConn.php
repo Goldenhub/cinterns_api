3c69203b6f86efa6aab45650495b8b2add046158
goldenhub <goldenazubuike@gmail.com>
2022-07-01 12:55:47 +0100

    setting environment variables

--- api/dbConn.php
+++ api/dbConn.php
@@ -8,27 +8,32 @@ class Conn
     protected $user;
     protected $pass;
     protected $db;
-    protected $active_group;
-    protected $query_builder;
 
     protected $conn;
 
+    public function __construct($cleardb_url, $host, $user, $pass, $db)
+    {
+        $this->cleardb_url = $cleardb_url;
+        $this->host = $host;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->db = $db;
+    }
+
     protected function connect()
     {
-        if ($_SERVER['HTTP_HOST'] != 'localhost') {
-            $this->cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-            $this->host = $this->cleardb_url["host"];
-            $this->user = $this->cleardb_url["user"];
-            $this->pass = $this->cleardb_url["pass"];
-            $this->db = substr($this->cleardb_url["path"], 1);
-            $this->active_group = 'default';
-            $this->query_builder = TRUE;
-        } else {
-            $this->host = 'localhost';
-            $this->user = 'root';
-            $this->pass = '';
-            $this->db = 'cinterns';
-        }
+        // if ($_SERVER['HTTP_HOST'] != 'localhost') {
+        //     $this->cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        //     $this->host = $this->cleardb_url["host"];
+        //     $this->user = $this->cleardb_url["user"];
+        //     $this->pass = $this->cleardb_url["pass"];
+        //     $this->db = substr($this->cleardb_url["path"], 1);
+        // } else {
+        //     $this->host = 'localhost';
+        //     $this->user = 'root';
+        //     $this->pass = '';
+        //     $this->db = 'cinterns';
+        // }
 
 
         $conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
