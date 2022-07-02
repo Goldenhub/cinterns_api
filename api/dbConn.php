ba79c7dd467ab3ed0732be80e51c4c1bb833a99b
Azubuike Chibueze Golden <60342841+Goldenhub@users.noreply.github.com>
2022-07-02 16:23:46 +0100

    removed unnecessary comments

--- api/dbConn.php
+++ api/dbConn.php
@@ -2,9 +2,7 @@
 <?php
 
 class Conn
-{
-    // protected $cleardb_url;
-    
+{    
     protected $host;
     protected $user;
     protected $pass;
@@ -14,7 +12,6 @@ class Conn
 
     public function __construct()
     {
-        // $this->cleardb_url = $cleardb_url;
         $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
         $this->host = $cleardb_url["host"];
         $this->user = $cleardb_url["user"];
@@ -24,19 +21,6 @@ class Conn
 
     protected function connect()
     {
-        // if ($_SERVER['HTTP_HOST'] != 'localhost') {
-        //     $this->cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        //     $this->host = $this->cleardb_url["host"];
-        //     $this->user = $this->cleardb_url["user"];
-        //     $this->pass = $this->cleardb_url["pass"];
-        //     $this->db = substr($this->cleardb_url["path"], 1);
-        // } else {
-        //     $this->host = 'localhost';
-        //     $this->user = 'root';
-        //     $this->pass = '';
-        //     $this->db = 'cinterns';
-        // }
-
         $conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
         $this->conn = $conn;
 
